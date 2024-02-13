13d627eb338cc0a5a5a9440521a5fbfc6c212467
0xsadik <torikussadik404@gmail.com>
2024-02-13 11:56:32 +0600

    how to print

---
+++ print.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define sesh '\n';
+
+int main()
+{
+    vector<int> v;
+
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+
+    cout << v[0] << sesh;
+    cout << v[3] << sesh;
+
+    return 0;
+}
